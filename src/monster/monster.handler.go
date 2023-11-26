ef3d91fa4c6c770222ac7f33e7f233e6ecefd8cd
Antoine <a.fourcade65@gmail.com>
2023-11-26 13:25:52 +0100

    feat(monster): monster portraits are now displayed and (WIP) edit monster form close to completion

--- src/monster/monster.handler.go
+++ src/monster/monster.handler.go
@@ -1,6 +1,7 @@
 package monster
 
 import (
+	"encoding/base64"
 	"fmt"
 	"html/template"
 	"io"
@@ -17,10 +18,6 @@ import (
 
 const fileBasePath = "./templates/monster/"
 
-type monsterTemplateData struct {
-	Monsters []models.Monster
-}
-
 type traitsDto struct {
 	Name        string
 	Description string
@@ -32,7 +29,7 @@ type actionDto struct {
 	Type        string
 }
 
-type monsterDto struct {
+type createMonsterDto struct {
 	Campaign          uint
 	Name              string
 	Type              string
@@ -68,10 +65,18 @@ type monsterDto struct {
 	Portrait          []byte
 }
 
+type serializedMonsterDto struct {
+	ID        uint
+	Name      string
+	Type      string
+	SubType   string
+	Challenge string
+	Portrait  string
+}
+
 var decoder = schema.NewDecoder()
 
 func getAllMonsters(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	var tD monsterTemplateData
 	var monsters []models.Monster
 	db := db.GetDbConnection()
 
@@ -84,9 +89,50 @@ func getAllMonsters(w http.ResponseWriter, r *http.Request, _ httprouter.Params)
 		return
 	}
 
-	tD.Monsters = monsters
-	t := template.Must(template.ParseFiles(filepath.Join(fileBasePath, "AllMonsters.html")))
-	t.Execute(w, tD)
+	var monsterDtos []serializedMonsterDto
+	for _, monster := range monsters {
+		var b64Portrait string
+
+		mimeType := http.DetectContentType(monster.Portrait)
+
+		switch mimeType {
+		case "image/jpeg":
+			b64Portrait += "data:image/jpeg;base64,"
+		case "image/png":
+			b64Portrait += "data:image/png;base64,"
+		case "image/webp":
+			b64Portrait += "data:image/webp;base64,"
+		}
+
+		b64Portrait += base64.StdEncoding.EncodeToString(monster.Portrait)
+
+		dto := serializedMonsterDto{
+			ID:        monster.ID,
+			Name:      monster.Name,
+			Type:      monster.Type,
+			SubType:   monster.SubType,
+			Challenge: monster.Challenge,
+			Portrait:  b64Portrait,
+		}
+
+		monsterDtos = append(monsterDtos, dto)
+	}
+
+	data := map[string][]serializedMonsterDto{
+		"Monsters": monsterDtos,
+	}
+
+	funcMap := template.FuncMap{
+		"safe": func(s string) template.URL {
+			return template.URL(s)
+		},
+	}
+
+	t := template.Must(template.New("AllMonsters.html").Funcs(funcMap).ParseFiles(filepath.Join(fileBasePath, "AllMonsters.html")))
+	err := t.Execute(w, data)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
 }
 
 func getNewMonsterTemplate(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -133,16 +179,21 @@ func getNewMonsterActions(w http.ResponseWriter, r *http.Request, ps httprouter.
 	t.Execute(w, data)
 }
 
-func createMonster(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func createOrUpdateMonster(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	defer r.Body.Close()
 
-	err := r.ParseMultipartForm(10 << 20)
+	monsterId, err := strconv.Atoi(ps.ByName("id"))
+	if err != nil {
+		fmt.Print(err.Error(), monsterId)
+	}
+
+	err = r.ParseMultipartForm(10 << 20)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	var monster monsterDto
+	var monster createMonsterDto
 
 	err = decoder.Decode(&monster, r.PostForm)
 	if err != nil {
@@ -181,52 +232,62 @@ func createMonster(w http.ResponseWriter, r *http.Request, _ httprouter.Params) 
 	monsterEntity.MasteryBonus = monster.MasteryBonus
 	monsterEntity.Description = monster.Description
 
-	if monster.SpecialTraits[0].Name != "" {
-		for _, st := range monster.SpecialTraits {
-			newSpecialTrait := models.SpecialTrait{}
-			newSpecialTrait.Name = st.Name
-			newSpecialTrait.Description = st.Description
-			monsterEntity.SpecialTraits = append(monsterEntity.SpecialTraits, newSpecialTrait)
+	if len(monster.SpecialTraits) > 0 {
+		if monster.SpecialTraits[0].Name != "" {
+			for _, st := range monster.SpecialTraits {
+				newSpecialTrait := models.SpecialTrait{}
+				newSpecialTrait.Name = st.Name
+				newSpecialTrait.Description = st.Description
+				monsterEntity.SpecialTraits = append(monsterEntity.SpecialTraits, newSpecialTrait)
+			}
 		}
 	}
 
-	if monster.Actions[0].Name != "" {
-		for _, ac := range monster.Actions {
-			newAction := models.Action{}
-			newAction.Name = ac.Name
-			newAction.Description = ac.Description
-			newAction.Type = "Action"
-			monsterEntity.Actions = append(monsterEntity.Actions, newAction)
+	if len(monster.Actions) > 0 {
+		if monster.Actions[0].Name != "" {
+			for _, ac := range monster.Actions {
+				newAction := models.Action{}
+				newAction.Name = ac.Name
+				newAction.Description = ac.Description
+				newAction.Type = "action"
+				monsterEntity.Actions = append(monsterEntity.Actions, newAction)
+			}
 		}
 	}
 
-	if monster.Reactions[0].Name != "" {
-		for _, re := range monster.Reactions {
-			newReaction := models.Action{}
-			newReaction.Name = re.Name
-			newReaction.Description = re.Description
-			newReaction.Type = "Réaction"
-			monsterEntity.Actions = append(monsterEntity.Actions, newReaction)
+	if len(monster.Reactions) > 0 {
+		if monster.Reactions[0].Name != "" {
+			for _, re := range monster.Reactions {
+				newReaction := models.Action{}
+				newReaction.Name = re.Name
+				newReaction.Description = re.Description
+				newReaction.Type = "reaction"
+				monsterEntity.Actions = append(monsterEntity.Actions, newReaction)
+			}
 		}
 	}
 
-	if monster.BonusActions[0].Name != "" {
-		for _, bac := range monster.BonusActions {
-			newBonusAction := models.Action{}
-			newBonusAction.Name = bac.Name
-			newBonusAction.Description = bac.Description
-			newBonusAction.Type = "Action bonus"
-			monsterEntity.Actions = append(monsterEntity.Actions, newBonusAction)
+	if len(monster.BonusActions) > 0 {
+		if monster.BonusActions[0].Name != "" {
+			for _, bac := range monster.BonusActions {
+				newBonusAction := models.Action{}
+				newBonusAction.Name = bac.Name
+				newBonusAction.Description = bac.Description
+				newBonusAction.Type = "bonus"
+				monsterEntity.Actions = append(monsterEntity.Actions, newBonusAction)
+			}
 		}
 	}
 
-	if monster.LegendaryActions[0].Name != "" {
-		for _, lac := range monster.LegendaryActions {
-			newLegendaryAction := models.Action{}
-			newLegendaryAction.Name = lac.Name
-			newLegendaryAction.Description = lac.Description
-			newLegendaryAction.Type = "Action légendaires"
-			monsterEntity.Actions = append(monsterEntity.Actions, newLegendaryAction)
+	if len(monster.LegendaryActions) > 0 {
+		if monster.LegendaryActions[0].Name != "" {
+			for _, lac := range monster.LegendaryActions {
+				newLegendaryAction := models.Action{}
+				newLegendaryAction.Name = lac.Name
+				newLegendaryAction.Description = lac.Description
+				newLegendaryAction.Type = "legendary"
+				monsterEntity.Actions = append(monsterEntity.Actions, newLegendaryAction)
+			}
 		}
 	}
 
@@ -248,15 +309,13 @@ func createMonster(w http.ResponseWriter, r *http.Request, _ httprouter.Params) 
 
 	monsterEntity.Portrait = fileBytes
 
-	if result := db.Create(&monsterEntity); result.Error != nil {
+	if result := db.Save(&monsterEntity); result.Error != nil {
 		http.Error(w, result.Error.Error(), http.StatusBadRequest)
 		return
 	}
-
-	fmt.Println("Monstre créé")
 }
 
-func editMonsterTemplate(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func getEditMonsterTemplate(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	skillId, err := strconv.Atoi(ps.ByName("id"))
 
 	if err != nil {
@@ -287,7 +346,7 @@ func editMonsterTemplate(w http.ResponseWriter, r *http.Request, ps httprouter.P
 	t.Execute(w, data)
 }
 
-func editMonsterStats(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func getEditMonsterStats(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	monsterId, err := strconv.Atoi(ps.ByName("id"))
 
 	if err != nil {
@@ -308,6 +367,66 @@ func editMonsterStats(w http.ResponseWriter, r *http.Request, ps httprouter.Para
 
 	t := template.Must(template.ParseFiles(filepath.Join(fileBasePath, "/fragments/EditMonster/Stats.html")))
 	t.Execute(w, data)
+}
+
+func getEditMonsterSkills(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	monsterId, err := strconv.Atoi(ps.ByName("id"))
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var monsterSkills []models.SpecialTrait
+	db := db.GetDbConnection()
+
+	if result := db.Where("monster_id = ?", monsterId).Find(&monsterSkills); result.Error != nil {
+		fmt.Println(result.Error)
+	}
+
+	next := len(monsterSkills) + 1
+
+	data := map[string]interface{}{
+		"Skills": monsterSkills,
+		"next":   next,
+	}
+
+	t := template.Must(template.ParseFiles(filepath.Join(fileBasePath, "/fragments/EditMonster/Skills.html")))
+	err = t.Execute(w, data)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+}
+
+func getEditMonsterActions(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	monsterId, err := strconv.Atoi(ps.ByName("id"))
+	actionType := ps.ByName("type")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var monsterActions []models.Action
+	db := db.GetDbConnection()
+
+	if result := db.Where("monster_id = ? AND type = ?", monsterId, actionType).Find(&monsterActions); result.Error != nil {
+		fmt.Println(result.Error)
+	}
+
+	next := len(monsterActions) + 1
+
+	data := map[string]interface{}{
+		"Actions": monsterActions,
+		"next":    next,
+		"type":    actionType,
+	}
+
+	t := template.Must(template.ParseFiles(filepath.Join(fileBasePath, "/fragments/EditMonster/Actions.html")))
+	err = t.Execute(w, data)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
 }
 
 func deleteCurrentAbility(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
--- src/monster/monster.router.go
+++ src/monster/monster.router.go
@@ -9,12 +9,14 @@ func ConfigureRoutes(r *httprouter.Router) {
 	r.GET("/monster/skills/new/:id", getNewMonsterSkills)
 	r.GET("/monster/actions/new/:id/:type", getNewMonsterActions)
 
-	r.GET("/monster/edit/:id", editMonsterTemplate)
-	r.GET("/monster/stats/edit/:id", editMonsterStats)
-	// r.GET("/monster/skills/edit")
-	// r.GET("/monster/actions/edit")
+	r.GET("/monster/edit/:id", getEditMonsterTemplate)
+	r.GET("/monster/stats/edit/:id", getEditMonsterStats)
+	r.GET("/monster/skills/edit/:id", getEditMonsterSkills)
+	r.GET("/monster/actions/edit/:id/:type", getEditMonsterActions)
 
-	r.POST("/monster/new", createMonster)
+	r.POST("/monster/new", createOrUpdateMonster)
+
+	r.PUT("/monster/edit/:id", createOrUpdateMonster)
 
 	r.DELETE("/monster/skills/new", deleteCurrentAbility)
 	r.DELETE("/monster/actions/new", deleteCurrentAbility)
